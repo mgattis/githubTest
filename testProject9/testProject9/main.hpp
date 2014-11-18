031b0c99c6ec4f2854371442fd0b422663ea3d16
mgattis <ryangattis@sbcglobal.net>
2014-11-17 21:45:55 -0600

    Added actuall project.

---
+++ testProject9/testProject9/main.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#ifndef _MAIN_HPP
+#define _MAIN_HPP
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+#include <list>
+#include <vector>
+#include <glm/glm.hpp>
+#include <SDL.h>
+#include <gl/glew.h>
+
+#ifndef M_PI
+#define M_PI 3.1415926535897932384626433832795
+#endif
+
+#endif /* !_MAIN_HPP */
